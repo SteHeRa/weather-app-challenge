595ded9b0c7124382c977c4240e5f9dbdb3542bb
Ste Head-Rapson <stephenheadrapson@hotmail.co.uk>
2020-11-13 16:46:23 +0000

    feat: show saved cities

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 
 import WeatherList from './containers/WeatherList';
 import Search from './containers/Search';
+import ApiService from './services/ApiService';
 
 const App = () => {
   const [cityList, setCityList] = useState([]);
+
+  const getCityList = async (userId) => {
+    const res = await ApiService.getCities(userId);
+    setCityList(res);
+  };
+
+  useEffect(() => {
+    getCityList(process.env.TEST_USER_ID);
+  }, []);
 
   return (
     <React.StrictMode>
