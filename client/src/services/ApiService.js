23008fc8b93abefa24f7888ade77c2e9695bbfdc
Ste Head-Rapson <stephenheadrapson@hotmail.co.uk>
2020-11-13 16:45:39 +0000

    feat: wrote client api functions

--- client/src/services/ApiService.js
+++ client/src/services/ApiService.js
@@ -1,9 +1,30 @@
-const fetchWeather = (city) => {
+const BASE_URL = process.env.SERVER_URL;
+
+const fetchWeather = async (city) => {
   return fetch(
     `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${process.env.OWM_API_KEY}`
   ).then((res) => res.json());
 };
 
+const getCities = async (userId) => {
+  const cities = await fetch(
+    `${BASE_URL}/cities?userId=${userId}`
+  ).then((res) => res.json());
+  return cities;
+};
+
+const postCity = async (userId, city) => {
+  return fetch(`${BASE_URL}/cities`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ userId: userId, city: city }),
+  }).then((res) => res.json());
+};
+
 module.exports = {
   fetchWeather,
+  getCities,
+  postCity,
 };
