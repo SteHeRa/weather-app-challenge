238747c65f9235afefc61344d282d52c7f92d628
Ste Head-Rapson <stephenheadrapson@hotmail.co.uk>
2020-11-14 16:25:07 +0000

    feat: able to select date and time

--- client/src/components/WeatherListItem.js
+++ client/src/components/WeatherListItem.js
@@ -1,31 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import WeatherListItemContent from './WeatherListItemContent';
+import useDropdown from '../hooks/useDropdown';
+import dayjs from 'dayjs';
 
 const WeatherListItem = ({ weather }) => {
+  const [days, setDays] = useState([]);
+  const [day, DayDropdown] = useDropdown('Day', 0, days);
+  const [hours, setHours] = useState([]);
+  const [time, TimeDropdown] = useDropdown('Time', 0, hours);
+
+  useEffect(() => {
+    if (weather) {
+      setDays(
+        weather.weather.map((day) =>
+          dayjs(day[0].dt * 1000).format('ddd DD-MMM-YYYY')
+        )
+      );
+      setHours(
+        weather.weather[day].map((time) =>
+          dayjs(time.dt * 1000).format('HH:mm')
+        )
+      );
+    }
+  }, [day, weather]);
+
   return weather ? (
     <div className="col s3 m4">
-      <div className="card">
+      <div className="card blue-grey lighten-4">
         <div className="card-title">
-          <span>{weather.city.name}</span>
+          <span>{weather.name}</span>
         </div>
-        <div className="card-content">
-          <div className="col s12">
-            <div className="col s4 offset-s3">
-              <img
-                src={`http://openweathermap.org/img/wn/${weather.list[0].weather[0].icon}@2x.png`}
-                alt="weather-icon"
-              />
-            </div>
-          </div>
-          <ul>
-            <li>{new Date(weather.list[0].dt * 1000).toUTCString()}</li>
-            <li>
-              {weather.list[0].weather[0].main}:{' '}
-              {weather.list[0].weather[0].description}
-            </li>
-            <li>Temp: {weather.list[0].main.temp}&deg;C</li>
-            <li>Humidity: {weather.list[0].main.humidity}%</li>
-          </ul>
+        <div>
+          <DayDropdown />
+          <TimeDropdown />
         </div>
+        <WeatherListItemContent weather={weather.weather[day][time]} />
       </div>
     </div>
   ) : null;
